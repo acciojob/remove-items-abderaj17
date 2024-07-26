75a7c72ff1d72b279373ae17ce83fbf0c203ce28
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-07-26 16:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let selectedItem = document.getElementById("colorSelect");
+
+let btn = document.querySelector("input[type='button']");
+
+btn.addEventListener('click', removeItem)
+
+function removeItem() {
+	let selectedOption = selectedItem.selectedOptions[0];
+	selectedItem.removeChild(selectedOption);
+}
